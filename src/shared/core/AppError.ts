0f1c2f6ebe6315d6f038b59e1e3f1ff55bddd46f
oluwaseun <seun@chaka.ng>
2020-09-28 11:49:55 +0100

    Added elasticsearch logger

--- src/shared/core/AppError.ts
+++ src/shared/core/AppError.ts
@@ -1,19 +1,19 @@
-import { Result } from "./Result";
-import { UseCaseError } from "./UseCaseError";
+import logger from '../core/Logger';
+import { Result } from './Result';
+import { UseCaseError } from './UseCaseError';
 
 export namespace AppError {
 	export class UnexpectedError extends Result<UseCaseError> {
-		public constructor (err:any) {
+		public constructor(err: any) {
 			super(false, {
 				message: 'An unexpected error occured.',
-				error: err
-			} as UseCaseError)
-			console.log(`[AppError]: An unexpected error occured`)
-			console.error(err)
+				error: err,
+			} as UseCaseError);
+			logger.error(`[AppError]: An unexpected error occured`, err);
 		}
 
 		public static create(err: any): UnexpectedError {
-			return new UnexpectedError(err)
+			return new UnexpectedError(err);
 		}
 	}
 }
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,17 +3,23 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import { v1Router } from './api/v1';
+import logger, { stream } from '../../core/Logger';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(
+	morgan(
+		`:remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms ":referrer" ":user-agent"`,
+		{ stream }
+	)
+);
 
 app.use('/api/v1', v1Router);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	logger.info(`Example app listening at http://localhost:${port}`);
 });
